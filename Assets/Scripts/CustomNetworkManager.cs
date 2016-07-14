8c99263c35283f77ad3901ca826b56c6122fc40b
Alejandro Font <Alejandro Font>
2016-07-14 01:35:02 -0700

    -Solution Cleanup. -Added base networking, supports custom character for host (prop for boss, can be moved around with input axis) -Modified Mob Character prefab to support newtorking.

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+class CustomNetworkManager : NetworkManager
+{
+    
+    public GameObject bossPrefab { get; set; }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        if (conn.hostId == -1)
+        {
+            GameObject Boss = GameObject.Find("BossBase");
+            NetworkServer.AddPlayerForConnection(conn, Boss, playerControllerId);
+        }
+        else
+        {
+            base.OnServerAddPlayer(conn, playerControllerId);
+        }
+    }
+}
---
+++ Assets/VerySimpleMoveScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class VerySimpleMoveScript : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isLocalPlayer)
+        {
+            var pos = transform.position;
+
+            pos.x += Input.GetAxis("Horizontal");
+            pos.z += Input.GetAxis("Vertical");
+
+            transform.position = pos;
+        }
+	}
+}
