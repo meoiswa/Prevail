e8d3935603f7cff2c6cd1ac236c5146708484362
Sopbucket <BrainiacDK@gmail.com>
2016-07-16 14:53:21 -0600

    asdf

---
+++ Assets/MobQuirkRunSpeed.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class MobQuirkRunSpeed : MonoBehaviour {
+    private Collider other;
+    public GameObject mobcontroller;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public IEnumerator SpeedUp(RigidbodyFirstPersonController.MovementSettings settings)
+    {
+        Debug.Log("You picked up a speed quirk");
+        settings.ForwardSpeed *= 1.5f;
+        settings.BackwardSpeed *= 1.5f;
+        settings.StrafeSpeed *= 1.5f;
+        Debug.Log("Forward speed is now" + settings.ForwardSpeed);
+
+        yield return new WaitForSeconds(5f);
+        //Debug.Log("Resetting to default values");
+        //mobcontroller.GetComponent<RigidbodyFirstPersonController>().movementSettings.ForwardSpeed /= 1.5f;
+        //mobcontroller.GetComponent<RigidbodyFirstPersonController>().movementSettings.BackwardSpeed /= 1.5f;
+        //mobcontroller.GetComponent<RigidbodyFirstPersonController>().movementSettings.StrafeSpeed /= 1.5f;
+        yield return null;
+
+    }
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(SpeedUp(mobcontroller.GetComponent<RigidbodyFirstPersonController>().movementSettings));
+        }
+    }
+	void Update ()
+    {
+	   
+	}
+}
