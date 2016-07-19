66b32d37f4bd4db084d4a83ec00811316d729e24
Alejandro Font <Alejandro Font>
2016-07-19 05:10:34 -0600

    -Added new prefabs to city generation script -Added networking to city generation script (syncs seed on start) -Added multiplayer lobby logic

---
+++ Assets/Scripts/CustomNetworkLobbyManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class CustomNetworkLobbyManager : NetworkLobbyManager
+{
+    public GameObject bossPrefab { get; set; }
+    
+    public override GameObject OnLobbyServerCreateGamePlayer(NetworkConnection conn, short playerControllerId)
+    {
+        if (conn.hostId == -1)
+        {
+
+            return (GameObject) GameObject.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        }
+        else
+        {
+            return base.OnLobbyServerCreateGamePlayer(conn, playerControllerId);
+        }
+    }
+}
