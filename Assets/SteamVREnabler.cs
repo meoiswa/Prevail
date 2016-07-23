24ecee3c5cae1fd196fac9fdd8f0343adf8c6ea8
Alejandro Font <Alejandro Font>
2016-07-22 22:42:06 -0600

    SteamVR enabler script

---
+++ Assets/SteamVREnabler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System;
+
+public class SteamVREnabler : MonoBehaviour {
+
+    void Awake()
+    {
+        if (Application.isEditor || !Environment.GetCommandLineArgs().Any(s => s == "-steamvr"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
