65b1ec41821973c74453ff2940ed179a66af5fa8
Alejandro Font <Alejandro Font>
2016-08-03 14:31:10 -0600

    New improved NetCharacter

---
+++ Assets/Scripts/TestCharacterSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCharacterSpawner : MonoBehaviour {
+
+    public GameObject PlayerNetControllerPrefab;
+    public GameObject PlayerNetCharacterPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+        
+        var ctrl = ((GameObject)Instantiate(PlayerNetControllerPrefab, Vector3.up * 10f, Quaternion.identity)).GetComponent<PlayerNetController>();
+        var chara = ((GameObject)Instantiate(PlayerNetCharacterPrefab, Vector3.up * 10f, Quaternion.identity)).GetComponent<PlayerNetCharacter>();
+
+        ctrl.Character = chara;
+        chara.Controller = ctrl;
+
+        ctrl.offline = true;
+        ctrl.GameStarted = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
