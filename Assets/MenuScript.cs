470f2df17554dc66939bc8c729a96cb67002e250
Alejandro Font <Alejandro Font>
2016-08-04 14:22:45 -0600

    Menu for Client, ultra quality shadows

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MenuScript : MonoBehaviour {
+
+    public InputField nameField;
+    public InputField ipField;
+
+    public GameObject Panel;
+    
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void OnButtonConnect()
+    {
+        PrevailClient.Instance.PlayerName = nameField.text;
+        PrevailClient.Instance.networkAddress = ipField.text;
+        PrevailClient.Instance.StartClient();
+        Panel.SetActive(false);
+    }
+}
